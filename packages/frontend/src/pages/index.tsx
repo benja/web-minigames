531e8c178c3bf7d9d8bd979ce753762bf51586df
Matthew Holmes <matt.holmes343@gmail.com>
2021-05-06 21:45:30 +0200

    Limitations for joining a lobby

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -18,9 +18,12 @@ export default function Index() {
   const [copied, setCopied] = useState(false);
   const [message, setMessage] = useState('');
 
-  const { data: games } = useGames();
   const { state } = useContext(StoreContext);
   const { lobbyId } = router.query;
+
+  const { data: games } = useGames({
+    limit: state.lobby && state.lobby.players.length
+  });
 
   useEffect(() => {
     if (!state.socket || state.lobby) return;
