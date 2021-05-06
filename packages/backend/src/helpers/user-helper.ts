8e6af543b43da480c5f17752ec66eb17bd2f36b5
Benjamin Akar <benjaminakar2001@gmail.com>
2021-05-06 14:30:58 +0200

    fix leave bug

--- packages/backend/src/helpers/user-helper.ts
+++ packages/backend/src/helpers/user-helper.ts
@@ -1,13 +1,13 @@
 import { Socket } from 'socket.io';
-import { deleteClient, getClientById } from "../client-manager";
-import LobbyHelper from "./lobby-helper";
+import { deleteClient, getClientById } from '../client-manager';
+import LobbyHelper from './lobby-helper';
 
 export default class UserHelper {
   public static disconnect(socket: Socket) {
     const client = getClientById(socket.id);
 
     if (client.currentLobby) {
-     LobbyHelper.leave(socket);
+      LobbyHelper.leave(socket);
     }
 
     deleteClient(socket);
