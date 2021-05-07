bd137a3fd266f63a039a1a3116623274831e476c
Matthew Holmes <matt.holmes343@gmail.com>
2021-05-07 23:59:35 +0200

    Game type

--- packages/backend/src/modules/draw-it/listeners/GameMessage.ts
+++ packages/backend/src/modules/draw-it/listeners/GameMessage.ts
@@ -1,14 +1,14 @@
-import { DrawItSocketEvents, GameTypes } from "@wmg/shared";
-import { SocketUser } from "../../../client-manager";
-import { GameListener } from "../../game-listener";
-import { GameCore } from "../../game-core";
+import { DrawItSocketEvents } from '@wmg/shared';
+import { SocketUser } from '../../../client-manager';
+import { GameListener } from '../../game-listener';
+import { DrawIt } from '../DrawIt';
 
 export class GameMessage extends GameListener {
   constructor() {
     super(DrawItSocketEvents.GAME_SEND_MESSAGE);
   }
 
-  async handle(user: SocketUser, game: GameCore<GameTypes.DRAWING>, message: string) {
+  async handle(user: SocketUser, game: DrawIt, message: string) {
     game.emitToAll(DrawItSocketEvents.GAME_SEND_MESSAGE, message);
   }
 }
