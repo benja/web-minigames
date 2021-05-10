e638eca3aa2ac2514af9f17dcfc033e597df6c10
Matthew Holmes <matt.holmes343@gmail.com>
2021-05-10 21:32:35 +0200

    Fallback

--- packages/frontend/src/ui/components/molecules/IconInput/IconInput.tsx
+++ packages/frontend/src/ui/components/molecules/IconInput/IconInput.tsx
@@ -11,23 +11,25 @@ interface IconButtonProps {
 }
 export function IconInput(props: IconButtonProps) {
   return (
-    <Container onSubmit={e => {
-      e.preventDefault();
-      props.onSubmit();
-    }}>
-      <Input text={props.text} onChange={props.onChange} />
+    <Container
+      onSubmit={e => {
+        e.preventDefault();
+        props.onSubmit();
+      }}
+    >
+      <Input text={props.text} onChange={props.onChange ? props.onChange : () => {}} />
       <IconContainer onClick={props.onClick}>
         <Icon icon={props.icon} />
       </IconContainer>
     </Container>
-  )
+  );
 }
 
 const Container = styled.form`
   display: flex;
   flex-direction: row;
   width: 100%;
-  
+
   > input {
     border-width: 1px 0 1px 1px;
     border-radius: 5px 0 0 5px;
@@ -38,6 +40,6 @@ const IconContainer = styled.div`
   padding: 10px 15px;
   border: 1px solid lightgray;
   border-width: 1px 1px 1px 0;
-  
+
   border-radius: 0 5px 5px 0;
 `;
