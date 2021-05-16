cfd140b7897d7b4721f39553c7bf90819fefd037
Benjamin Akar <benjaminakar2001@gmail.com>
2021-05-17 00:06:31 +0200

    backend change

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://web-minigames.vercel.app'],
+    origin: ['*'],
     optionsSuccessStatus: 200,
     credentials: true,
   }),
